3871998e7dc6243d0ec7093eb8b23c8501808a2e
süleyman mert tunç <suleymanmerttunc@hotmail.com>
2025-04-18 19:05:12 +0300

    GhostyEclipse

---
+++ GhostyModel1/src/main/java/com/example/ghostymodel/Main.java
@@ -0,0 +1,24 @@
+package com.example.ghostymodel;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("login.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 650, 550);
+        stage.setTitle("GHOSTY");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
